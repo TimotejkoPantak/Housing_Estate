e3346136466f6ce403b6c81ef492b3b91d4354aa
TimotejkoPantak <timopantakgit@gmail.com>
2020-11-30 17:52:37 +0100

    Add project files.

---
+++ Housing_Estate/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Housing_Estate
+{
+    class Person
+    {
+
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public int Years { get; set; }
+        
+        public Person(string firstName, string lastName, int years)
+        {
+            Firstname = firstName;
+            Lastname = lastName;
+            Years = years;
+        }
+
+        public void ToString()
+        {
+            Console.WriteLine($"{Firstname},{Lastname},{Years}");
+        }
+    }
+}
---
+++ Housing_Estate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Housing_Estate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("Jozko", "Mrkva", 27);
+            person1.ToString();
+        }
+    }
+}
